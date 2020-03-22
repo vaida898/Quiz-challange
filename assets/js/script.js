6dfddd78f5f730aeec592212ecb850de64324e61
vaida898 <freelpfm@gmail.com>
2020-03-22 20:42:27 +0000

    changed id names in selects for dropdown selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-fetch('https://opentdb.com/api.php?amount=25').then((res) => res.json()).then((data) => {
+fetch('https://opentdb.com/api_category.php').then((res) => res.json()).then((data) => {
                                let output = '<h8>Categories</h8>';
 for (let i = 0; i < data.results.length; i++) {
     document.getElementById("category-select").innerHTML += `<option>${data.results[i].category}</option>`;
+}
+});
+
+fetch('https://opentdb.com/api.php?amount=25').then((res) => res.json()).then((data) => {
+                               let output = '<h8>Difficulties</h8>';
+for (let i = 0; i < data.results.length; i++) {
+    document.getElementById("difficulty-select").innerHTML += `<option>${data.results[i].difficulty}</option>`;
 }
 });
