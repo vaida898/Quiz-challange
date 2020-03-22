2b5098045a04eb813e10dbb9a19194d4a23bc6b9
vaida898 <freelpfm@gmail.com>
2020-03-22 16:30:18 +0000

    amended API URL link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-fetch('https://opentdb.com/api.php?amount=10').then((res) => res.json()).then((data) => {
+fetch('https://opentdb.com/api.php?amount=25').then((res) => res.json()).then((data) => {
                                let output = '<h8>Categories</h8>';
-for (let i = 0; i < data.length; i++) {
-    document.getElementById("category-select").innerHTML += `<option>${data[i].category}</option>`;
+for (let i = 0; i < data.results.length; i++) {
+    document.getElementById("category-select").innerHTML += `<option>${data.results[i].category}</option>`;
 }
-
 });
