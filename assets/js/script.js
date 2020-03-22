f84040901ae222fdcd095cc2f43ee945f1b0b1a5
vaida898 <freelpfm@gmail.com>
2020-03-22 16:01:21 +0000

    added fetch to category-select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+fetch('https://opentdb.com/api.php?amount=10').then((res) => res.json()).then((data) => {
+                               let output = '<h8>Categories</h8>';
 for (let i = 0; i < data.length; i++) {
     document.getElementById("category-select").innerHTML += `<option>${data[i].category}</option>`;
 }
 
+});
