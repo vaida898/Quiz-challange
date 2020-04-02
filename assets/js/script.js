3945fd1e20fb113110c0ad49d926416f494aa106
vaida898 <freelpfm@gmail.com>
2020-04-02 20:08:11 +0000

    added difficulty levels to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,28 +12,10 @@ function selectCat() {
                     `<option value="${category.id}">${category.name}</option>`,
                 );
             });
-            console.log(data);
         })
 }
-           
-        
+                   
 selectCat();
-
-
-/*
-function myFunction() {
-  const categorySelect = $('#category-select');
-  fetch('https://opentdb.com/api_category.php')
-    .then(res => res.json())
-    .then(data => {
-      data.trivia_categories.forEach(category => {
-        categorySelect.append(
-          `<option value="${category.id}">${category.name}</option>`,
-        );
-      });
-    });
-}
-
 
 
 function getQuestions(categoryID, difficulty){
@@ -59,24 +41,3 @@ const select = $(".selectSomething");
 
 
 
-/*fetch('https://opentdb.com/api.php?amount=25').then((res) => res.json()).then((data) => {
-                               let output = '<h8>Categories</h8>';
-for (let i = 0; i < data.results.length; i++) {
-    document.getElementById("category-select").innerHTML += `<option>${data.results[i].category}</option>`;
-}
-});
-
-fetch('https://opentdb.com/api.php?amount=3').then((res) => res.json()).then((data) => {
-                                let output = '<h8>Difficulties</h8>';
-for (let i = 0; i < data.results.length; i++) {
-    document.getElementById("difficulty-select").innerHTML += `<option>${data.results[i].difficulty}</option>`;
-}
-});
-
-
-fetch('https://opentdb.com/api.php?amount=25').then((res) => res.json()).then((data) => {
-                               let output = '<h8>Questions</h8>';
-for (let i = 0; i < data.results.length; i++) {
-    document.getElementById("questions-select").innerHTML += `<option>${data.results[i].question}</option>`;
-}
-});*/
